fdeb6ba7de62c6d18059f75ee6c2c0a522dd129d
www-data <www-data@s2837717.elf.ict.griffith.edu.au>
2019-08-08 03:27:28 +1000

    fixes to task 3

--- index.php
+++ index.php
@@ -21,8 +21,10 @@
 </ul>
 <ul>
 		<li><a href="/webAppDev/week4/task1/blank/public">Week 4: Task 1</a></li>
-		<li><a href="/webAppDev/week4/task1/greetings/public/greeting">Week 4: Task 2 (Greeting)</a></li>
-		<li><a href="/webAppDev/week4/task1/greetings/public/user/Ben">Week 4: Task 2 (User Greeting)</a></li>
+		<li><a href="/webAppDev/week4/task2/greetings/public/greeting">Week 4: Task 2 (Greeting)</a></li>
+        <li><a href="/webAppDev/week4/task2/greetings/public/user/Ben">Week 4: Task 2 (User Greeting)</a></li>
+        <li><a href="/webAppDev/week4/task3/foreach/public/?title=PHP%20and%20MySQL&author=Simon%20Stobart%20and%20David%20Parsons&year=2008&url=www.cengage.co.uk/stobart/&pages=634">Week 4: Task 3</a></li>
+        
 </ul>
 
 
--- webAppDev/week4/task3/foreach/resources/views/foreach.blade.php
+++ webAppDev/week4/task3/foreach/resources/views/foreach.blade.php
@@ -3,25 +3,27 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Foreach loop example</title>
-		<link rel="stylesheet" href="{{asset('css/wp.css'}}" type="text/css">
+		<link rel="stylesheet" href="{{asset('css/wp.css')}}" type="text/css">
 	</head>
 
 	<body>
 		<table class="bordered">
-			<tr>
-				<th>Name</th>
-				<th>Value</th>
-			</tr>
-			@forelse ($get as $name => $value)
+			<tbody>
 				<tr>
-					<td>{{$name}}:</td>
-					<td>{{$value}}</td>
+					<th>Name</th>
+					<th>Value</th>
 				</tr>
-			@empty
-				<tr>
-					<td colspan="2">No URL variables</td>
-				</tr>
-			@endforelse
+				@forelse ($get as $name => $value)
+					<tr>
+						<td>{{$name}}:</td>
+						<td>{{$value}}</td>
+					</tr>
+				@empty
+					<tr>
+						<td colspan="2">No URL variables</td>
+					</tr>
+				@endforelse
+			</tbody>
 		</table>
 	</body>
 </html>
