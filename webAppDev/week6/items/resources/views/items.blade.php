d61f1f73690b5bd29c664c3f4b4f494b55a16413
www-data <www-data@s2837717.elf.ict.griffith.edu.au>
2019-08-29 00:25:23 +1000

    week6 items

--- webAppDev/week6/items/resources/views/items.blade.php
+++ webAppDev/week6/items/resources/views/items.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts/master')
+@extends('templates/main')
 
 @section('content')
 	@if (count($items))
 		<ul>
 		@foreach ($items as $item)
-			<li><a href="/public/items/{{$item['Id']}}">{{$item['Name']}}</a></li>
+			<li><a href="/webAppDev/week6/items/public/items/{{$item->id}}">{{$item->summary}}</a></li>
 		@endforeach
 		</ul>
 	@else
-		<p>No items exist. <a href="/public/create">Click here to create one.</a></p>
+		<p>No items exist. <a href="/webAppDev/week6/items/public/create">Click here to create one.</a></p>
 	@endif
 @endsection
 
