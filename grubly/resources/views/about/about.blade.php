68c4becd1a9cac448c4b57eb45c20d699f19f48d
www-data <www-data@s2837717.elf.ict.griffith.edu.au>
2019-09-29 06:10:52 +1000

    ER resize

--- grubly/resources/views/about/about.blade.php
+++ grubly/resources/views/about/about.blade.php
@@ -5,7 +5,7 @@
 	<section style="padding: 50px;">
 		<h2>Documentation</h2>
 		<p>ER Diagram Via embed:</p>
-		<iframe width="560" height="315" src='https://dbdiagram.io/embed/5d8e1e41ff5115114db4b1ab'> </iframe>
+		<iframe width="1120" height="630" src='https://dbdiagram.io/embed/5d8e1e41ff5115114db4b1ab'> </iframe>
 		
 		<h2>Notes about Grubly</h2>
 		<p>
